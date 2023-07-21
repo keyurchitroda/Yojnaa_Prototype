e46c194df3027db450a78d7e224cc395bcce78fe
keyurchitroda <keyurchitroda@gmail.com>
2023-07-21 15:39:54 +0530

    commit

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classnames from "classnames";
 import { usePagination, DOTS } from "./usePagination";
-// import "./Page.css";
+import "./Page.css";
 const Pagination = (props) => {
   const {
     onPageChange,
