7dfe947a448fd1b638fe702aeb2c694a09db2762
keyurchitroda <keyurchitroda@gmail.com>
2023-07-24 12:34:17 +0530

    commit 7/23/23

--- src/components/YojnaForm/FormsList.js
+++ src/components/YojnaForm/FormsList.js
@@ -1,20 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { API_URL } from "../config";
 import { useDispatch, useSelector } from "react-redux";
-import { ACNameList } from "../../redux/slices/yojnaformSlice";
+import {
+  ACNameList,
+  BoothNameList,
+  VibhagNameList,
+  clearVlaue,
+  setSearchValues,
+} from "../../redux/slices/yojnaformSlice";
+import { MagnifyingGlass, RotatingTriangles } from "react-loader-spinner";
+import _ from "lodash";
 
 const FormsList = () => {
+  const [acLoader, setAcLoader] = useState(false);
+  const [boothLoader, setBoothLoader] = useState(false);
+  const [vibhagLoader, setVibhagLoader] = useState(false);
+
   const dispatch = useDispatch();
 
-  const AcNameList = useSelector(
-    (state) => state.reducer.yojnaForms.acNameList
-  );
+  const yojnaForms = useSelector((state) => state.reducer.yojnaForms);
 
   useEffect(() => {
+    dispatch(clearVlaue());
     getAcNameList();
   }, []);
 
   const getAcNameList = () => {
+    setAcLoader(true);
     const requestOptions = {
       method: "GET",
       headers: { "Content-Type": "application/json" },
@@ -24,32 +36,55 @@ const FormsList = () => {
     fetch(apiUrl, requestOptions)
       .then((res) => res.json())
       .then(async (response) => {
-        await dispatch(ACNameList(response.data));
+        await dispatch(ACNameList(response));
+        setAcLoader(false);
       });
   };
 
-  const getBoothNameList = () => {
+  const getBoothNameList = (event) => {
+    setBoothLoader(true);
     const requestOptions = {
       method: "GET",
       headers: { "Content-Type": "application/json" },
     };
-    let apiUrl = `${API_URL}/schemesd/benificiary-records/`;
+    let apiUrl = `${API_URL}/schemesd/boothlistbyacno/?ac_no=${event.target.value}`;
 
     fetch(apiUrl, requestOptions)
       .then((res) => res.json())
-      .then((response) => {});
+      .then(async (response) => {
+        await dispatch(BoothNameList(response));
+        await dispatch(
+          setSearchValues({
+            ac_no: event.target.value,
+            booth_no: null,
+            village_name: "",
+          })
+        );
+        setBoothLoader(false);
+      });
   };
 
-  const getVibhagList = () => {
+  const getVibhagList = (event) => {
+    setVibhagLoader(true);
     const requestOptions = {
       method: "GET",
       headers: { "Content-Type": "application/json" },
     };
-    let apiUrl = `${API_URL}/schemesd/benificiary-records/`;
+    let apiUrl = `${API_URL}/schemesd/vibhagbyboothno/?ac_no=${yojnaForms.searchValues?.ac_no}&booth_no=${event.target.value}`;
 
     fetch(apiUrl, requestOptions)
       .then((res) => res.json())
-      .then((response) => {});
+      .then(async (response) => {
+        await dispatch(VibhagNameList(response));
+        await dispatch(
+          setSearchValues({ booth_no: event.target.value, village_name: "" })
+        );
+        setVibhagLoader(false);
+      });
+  };
+
+  const setOnchangeVibhagNo = async (event) => {
+    await dispatch(setSearchValues({ village_name: event.target.value }));
   };
 
   return (
@@ -58,36 +93,105 @@ const FormsList = () => {
         <div class="form-row">
           <div class="form-group col-md-4">
             <label for="exampleInputEmail1">AC Name</label>
-            <select class="custom-select" required>
-              <option value="" disabled selected>
-                Open this select menu
-              </option>
-              {AcNameList.map((items) => (
-                <option value={items.ac_no}>
-                  {items.eng_ac_name} ({items.ac_name})
+            {acLoader ? (
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <MagnifyingGlass
+                  visible={true}
+                  height="60"
+                  width="80"
+                  ariaLabel="MagnifyingGlass-loading"
+                  wrapperStyle={{}}
+                  wrapperClass="MagnifyingGlass-wrapper"
+                  glassColor="#c0efff"
+                  color="#e15b64"
+                />
+              </div>
+            ) : (
+              <select
+                class="custom-select"
+                required
+                onChange={getBoothNameList}
+              >
+                <option value="" disabled selected>
+                  Open this select menu
                 </option>
-              ))}
-            </select>
+                {_.map(_.get(yojnaForms, "acNameList", []), (items) => (
+                  <option value={items.ac_no}>{items.ac_name}</option>
+                ))}
+              </select>
+            )}
           </div>
           <div class="form-group col-md-4">
-            <label for="inputPassword4">Booth Naame</label>
-            <select class="custom-select" required>
-              <option value="">Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
+            <label for="inputPassword4">Booth Name</label>
+            {boothLoader ? (
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <MagnifyingGlass
+                  visible={true}
+                  height="60"
+                  width="80"
+                  ariaLabel="MagnifyingGlass-loading"
+                  wrapperStyle={{}}
+                  wrapperClass="MagnifyingGlass-wrapper"
+                  glassColor="#c0efff"
+                  color="#e15b64"
+                />
+              </div>
+            ) : (
+              <select
+                class="custom-select"
+                required
+                onChange={getVibhagList}
+                disabled={yojnaForms.searchValues.ac_no == null}
+              >
+                <option value="" disabled selected>
+                  Open this select menu
+                </option>
+                {_.map(_.get(yojnaForms, "boothNameList", []), (items) => (
+                  <option value={items.booth_no}>
+                    {items.eng_booth_name} ({items.booth_name})
+                  </option>
+                ))}
+              </select>
+            )}
           </div>
           <div class="form-group col-md-4">
             <label for="inputPassword4"> Vibhaag</label>
-            <select class="custom-select" required>
-              <option value="">Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
+            {vibhagLoader ? (
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <MagnifyingGlass
+                  visible={true}
+                  height="60"
+                  width="80"
+                  ariaLabel="MagnifyingGlass-loading"
+                  wrapperStyle={{}}
+                  wrapperClass="MagnifyingGlass-wrapper"
+                  glassColor="#c0efff"
+                  color="#e15b64"
+                />
+              </div>
+            ) : (
+              <select
+                class="custom-select"
+                required
+                disabled={
+                  yojnaForms.searchValues?.ac_no == null ||
+                  yojnaForms.searchValues?.booth_no == null
+                }
+                onChange={setOnchangeVibhagNo}
+              >
+                <option value="">Open this select menu</option>
+                {_.map(_.get(yojnaForms, "vibhagNameList", []), (items) => (
+                  <option value={items.village_name}>
+                    {items.eng_vibhag_name} ({items.vibhag_name})
+                  </option>
+                ))}
+              </select>
+            )}
           </div>
         </div>
+        <button type="submit" class="btn btn-primary">
+          Search
+        </button>
       </form>
     </div>
   );
--- src/redux/slices/yojnaformSlice.js
+++ src/redux/slices/yojnaformSlice.js
@@ -2,6 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   acNameList: [],
+  boothNameList: [],
+  vibhagNameList: [],
+  searchValues: {
+    ac_no: "",
+    booth_no: "",
+    village_name: "",
+  },
 };
 
 const formSlice = createSlice({
@@ -11,6 +18,15 @@ const formSlice = createSlice({
     acNameListSuccess: (state, action) => {
       state.acNameList = action.payload;
     },
+    boothNameListSuccess: (state, action) => {
+      state.boothNameList = action.payload;
+    },
+    vibhagNameListSuccess: (state, action) => {
+      state.vibhagNameList = action.payload;
+    },
+    searchValues: (state, action) => {
+      state.searchValues = { ...state.searchValues, ...action.payload }; // Update specific properties using spread
+    },
   },
 });
 
@@ -18,11 +34,50 @@ const formSlice = createSlice({
 export default formSlice.reducer;
 
 //Action
-const { acNameListSuccess } = formSlice.actions;
+const {
+  acNameListSuccess,
+  boothNameListSuccess,
+  searchValues,
+  vibhagNameListSuccess,
+} = formSlice.actions;
 
 export const ACNameList = (values) => async (dispatch) => {
   try {
     await dispatch(acNameListSuccess(values));
+  } catch (e) {
+    console.log("e", e);
+  }
+};
+
+export const BoothNameList = (values) => async (dispatch) => {
+  try {
+    await dispatch(boothNameListSuccess(values));
+  } catch (e) {
+    console.log("e", e);
+  }
+};
+
+export const VibhagNameList = (values) => async (dispatch) => {
+  try {
+    await dispatch(vibhagNameListSuccess(values));
+  } catch (e) {
+    console.log("e", e);
+  }
+};
+
+export const setSearchValues = (values) => async (dispatch) => {
+  try {
+    await dispatch(searchValues(values));
+  } catch (e) {
+    console.log("e", e);
+  }
+};
+
+export const clearVlaue = (values) => async (dispatch) => {
+  try {
+    await dispatch(acNameListSuccess([]));
+    await dispatch(boothNameListSuccess([]));
+    await dispatch(searchValues({}));
   } catch (e) {
     console.log("e", e);
   }
